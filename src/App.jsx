874e124a8a9113633d50ba416e863b00aa8fe308
maria <you@example.com>
2025-02-16 02:59:39 +0330

    add hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import NotFound from "./pages/NotFound";
@@ -19,18 +19,23 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
         <Toaster />
-        <Routes>
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/signin" element={<SigninPage />} />
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<Navigate to="category" replace />} />
-            <Route path="category" element={<CategoriesTab />} />
-            <Route path="products" element={<ProductsTab />} />
-            <Route path="orders" element={<OrdersTab />} />
-          </Route>
-          <Route path="/" element={<Navigate to="admin/category" replace />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <HashRouter>
+          <Routes>
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/signin" element={<SigninPage />} />
+            <Route path="/admin" element={<AdminLayout />}>
+              <Route index element={<Navigate to="category" replace />} />
+              <Route path="category" element={<CategoriesTab />} />
+              <Route path="products" element={<ProductsTab />} />
+              <Route path="orders" element={<OrdersTab />} />
+            </Route>
+            <Route
+              path="/"
+              element={<Navigate to="admin/category" replace />}
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </HashRouter>
       </QueryClientProvider>
     </DarkModeProvier>
   );
